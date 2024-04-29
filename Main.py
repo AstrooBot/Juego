27f3a37f13fd5022baa925e36b9231ae211992ea
AstrooBot <129632296+AstrooBot@users.noreply.github.com>
2024-04-29 07:59:00 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,35 +5,79 @@ size = width, height = 430,430
 screen = pygame.display.set_mode(size)
 pygame.init()
 
-class Lozas:
-    def __init__(self) -> None:
-        self.num_lozas = []
-        self.rect_lozas = []
-    def set_lozas(self, screen):
-        pygame.draw.rect(screen, 'white' , (j,i, 100, 100), 0, border_radius=15)
+class Loza:
+    def __init__(self, screen, x , y, color ) -> None:
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.color = color
+        self.shape = None
+        self.rect = None
+        self.text = None
+        self.set_loza()
         
-    def set_text(self,screen, j, i):
-        pass
+    def set_loza(self):
+
+        self.shape = pygame.draw.rect(self.screen, self.color , (self.x, self.y, 100, 100), border_radius=15)
+        self.rect = pygame.rect.Rect(self.shape)
+        if self.text != None:
+            self.screen.blit(self.text, (self.rect.centerx, self.rect.centery)) 
+
+        
        
     def update(self):
-         teclas = pygame.key.get_pressed()
-         pass
+        teclas = pygame.key.get_pressed()
+        if teclas[K_LEFT] and self.color == 'red':
+            self.x -= 115
+        if teclas[K_RIGHT] and self.color == 'red':
+            self.x += 115
+        if teclas[K_UP] and self.color == 'red':
+            self.y -= 115
+        if teclas[K_DOWN] and self.color == 'red':
+            self.y += 115     
+        self.set_loza()
+        
+            
+        
+        
+         
          
         
-        
+lozas_num = []        
 
     
-lozas = Lozas()
-lozas.set_lozas(screen)
+for j in range(0,3):
+    for i in range(0,3):
+        i = Loza(screen,  50+(j*15)+(100*j), 50+(i*15)+(100*i), ' white')
+        lozas_num.append(i)
 
+
+
+lozas_num[-1].color = 'red' 
+num = 0
+font = pygame.font.Font(None, 36)
+for i in lozas_num:
+    num +=1
+    i.text = font.render(str(num), 1, 'Black')
+    i.set_loza()
+    
+
+
+               
+ 
 while 1 :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            screen.fill('black')
+            for lozas in lozas_num:
+                lozas.update()
+       
         
-        lozas.update()
+       
         
-        pygame.display.update()
+    pygame.display.update()
 
 """Generar todas las cajas: for i in range(0,3):
             for j in range(0,3):
@@ -41,10 +85,10 @@ while 1 :
                 r = pygame.rect.Rect((50+(j*15)+(100*j), 50+(i*15)+(100*i), 50+(j*15)+(100*j)+100, 50+(i*15)+(100*i)+100))
                 self.rect_lozas.append( r )
                 self.num_lozas.append(s)"""
-"""Generar todos los textos:
-     num = 0
-        for i in self.num_lozas:
+"""num = 0
+font = pygame.font.Font(None, 36)
+for i in lozas_num:
             num +=1
             ctext = font.render(str(num), 1, 'Black')
-            screen.blit(ctext, (i.centerx, i.centery))
+            i.screen.blit(ctext, (i.rect.centerx, i.rect.centery))
     """
