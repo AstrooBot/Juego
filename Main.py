04ed6619239bd55b6e4fe6c960595026a91a57a9
astroobot <Beginner_Programmer@outlook.es>
2024-04-30 20:58:41 -0500

    uploading stable version

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import sys, pygame 
+import random
 from pygame.locals import *
 size = width, height = 430,430
 
@@ -14,7 +15,7 @@ class Loza:
         self.shape = None
         self.rect = None
         self.text = None
-        self.set_loza()
+        
         
     def set_loza(self):
 
@@ -25,26 +26,79 @@ class Loza:
 
         
        
-    def update(self):
+    def update(self,loza):
         teclas = pygame.key.get_pressed()
-        if teclas[K_LEFT] and self.color == 'red':
+
+        if teclas[K_LEFT] and 50 < self.x and check_left(loza, self.x, self.y) == True:
             self.x -= 115
-        if teclas[K_RIGHT] and self.color == 'red':
+
+        if teclas[K_RIGHT]  and self.x < 280 and check_right(loza, self.x, self.y) == True:
             self.x += 115
-        if teclas[K_UP] and self.color == 'red':
+
+
+        if teclas[K_UP] and 50 < self.y and check_up(loza, self.x, self.y) == True:
             self.y -= 115
-        if teclas[K_DOWN] and self.color == 'red':
-            self.y += 115     
+            
+
+        if teclas[K_DOWN]  and self.y < 280 and check_down(loza, self.x, self.y) == True :
+            self.y += 115
+            
+                
+
         self.set_loza()
         
             
+            
+def check_down(loza, x, y):
+        check = True
+
+        for i in loza:
+            if i.y == y+115 and i.x == x:
+                check = False
+            
+        return check 
+
+def check_up(loza, x, y):
         
+        check = True
+
+        for i in loza:
+            if i.y == y-115 and i.x == x:
+                check = False
+            
+        return check 
         
+def check_left(loza, x, y):
+        
+        check = True
+
+        for i in loza:
+            if i.y == y and i.x == x-115:
+                check = False
+            
+        return check
+
+def check_right(loza, x, y):
+        
+        check = True
+
+        for i in loza:
+            if i.y == y and i.x == x+115:
+                check = False
+            
+        return check     
+         
+    
+
+
          
          
         
 lozas_num = []        
-
+num = [1,2,3,4,5,6,7,8,9]
+index = 0
+random.shuffle(num)
+font = pygame.font.Font(None, 36)
     
 for j in range(0,3):
     for i in range(0,3):
@@ -53,42 +107,31 @@ for j in range(0,3):
 
 
 
-lozas_num[-1].color = 'red' 
-num = 0
+del lozas_num[-1] 
+num = [1,2,3,4,5,6,7,8]
+random.shuffle(num)
+index = 0
 font = pygame.font.Font(None, 36)
 for i in lozas_num:
-    num +=1
-    i.text = font.render(str(num), 1, 'Black')
-    i.set_loza()
     
+    i.text = font.render(str(num[index]), 1, 'Black')
+    index +=1
+    i.set_loza()
 
 
-               
- 
+
 while 1 :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             screen.fill('black')
+            
             for lozas in lozas_num:
-                lozas.update()
+                lozas.update(lozas_num)
        
         
        
         
     pygame.display.update()
 
-"""Generar todas las cajas: for i in range(0,3):
-            for j in range(0,3):
-                s = pygame.draw.rect(screen, 'white' , (50+(j*15)+(100*j), 50+(i*15)+(100*i), 100, 100), 0, border_radius=15) 
-                r = pygame.rect.Rect((50+(j*15)+(100*j), 50+(i*15)+(100*i), 50+(j*15)+(100*j)+100, 50+(i*15)+(100*i)+100))
-                self.rect_lozas.append( r )
-                self.num_lozas.append(s)"""
-"""num = 0
-font = pygame.font.Font(None, 36)
-for i in lozas_num:
-            num +=1
-            ctext = font.render(str(num), 1, 'Black')
-            i.screen.blit(ctext, (i.rect.centerx, i.rect.centery))
-    """
